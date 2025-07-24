858ff19292296ca9da73b06116953dcf3bde20be
Dylan <dylanyeowf@gmail.com>
2025-07-24 20:55:36 +0800

    updated routing to hash routing

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 import { createRoot } from 'react-dom/client'
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+import { HashRouter as Router, Routes, Route } from 'react-router-dom'
 import Layout from './Layout';
 // import { HomePage } from './homePage/HomePage'
 import LogInPage from './logInPage/LogInPage'
