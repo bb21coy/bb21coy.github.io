2ed1239841163ae826f529d173a2e9dcb06573c1
Dylan <dylanyeowf@gmail.com>
2025-09-16 16:25:18 +0800

    optimised performance by removing unused dependencies

--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,14 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { visualizer } from 'rollup-plugin-visualizer';
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [
     react(),
-  ],
-  build: {
-    sourcemap: true
-  }
+    visualizer({
+      open: true,            // open in browser after build
+      filename: 'stats.html' // optional
+    })
+  ]
 })
