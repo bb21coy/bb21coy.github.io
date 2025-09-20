b2a6d8c92548933ee976f7576769b6cc7ede5c09
Dylan <dylanyeowf@gmail.com>
2025-09-20 12:55:45 +0800

    optimised configurations

--- vite.config.js
+++ vite.config.js
@@ -3,12 +3,33 @@ import react from '@vitejs/plugin-react'
 import { visualizer } from 'rollup-plugin-visualizer';
 
 // https://vite.dev/config/
-export default defineConfig({
-  plugins: [
-    react(),
-    visualizer({
-      open: true,            // open in browser after build
-      filename: 'stats.html' // optional
-    })
-  ]
+export default defineConfig(({ mode }) => {
+  const isProd = mode === 'production'
+
+  return {
+    plugins: [
+      react(),
+      visualizer({
+        open: true,
+        filename: 'stats.html'
+      })
+    ],
+    server: {
+      host: true,
+      port: 5173,
+      hmr: {
+        host: '192.168.0.189'
+      }
+    },
+    resolve: {
+      alias: isProd
+        ? {
+            react: 'preact/compat',
+            'react-dom': 'preact/compat',
+            'react-dom/test-utils': 'preact/test-utils',
+            'react/jsx-runtime': 'preact/jsx-runtime'
+          }
+        : {}
+    }
+  }
 })
