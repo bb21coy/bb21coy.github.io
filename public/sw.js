3209c2d50dbc30d44c2dc03c3540712daab1bd3f
Dylan <dylanyeowf@gmail.com>
2025-09-30 19:54:21 +0800

    removed unused code

--- public/sw.js
+++ public/sw.js
@@ -1,4 +1,4 @@
-const version = "1.2.8";
+const version = "1.2.9";
 const CACHE_NAME = `bb21coy-cache-v${version}`;
 
 // These are the known root files and folders
--- vite.config.js
+++ vite.config.js
@@ -37,19 +37,12 @@ export default defineConfig(({ mode }) => {
     build: {
       chunkSizeWarningLimit: 1500,
       rollupOptions: {
-        input: {
-          main: 'index.html',
-          sw: './src/sw.js'
-        },
         output: {
           manualChunks: {
             firebase: ["@firebase/app", "@firebase/firestore", "@firebase/auth"]
           }
         }
       }
-    },
-    define: {
-      __BUILD_ID__: JSON.stringify(Date.now()),
     }
   }
 })
