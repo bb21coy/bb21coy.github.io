deca7a180fca9e91274f1d24b0cb62db73a7f9f1
Dylan <dylanyeowf@gmail.com>
2025-10-02 19:07:59 +0800

    removed unnecessary logging

--- src/attendanceManagementPage/ParadeAttendance.jsx
+++ src/attendanceManagementPage/ParadeAttendance.jsx
@@ -132,7 +132,7 @@ const ParadeAttendance = ({ parade, users }) => {
 									{!takingAttendance ?
 										<td>{currentAttendance[boy.id] || "-"}</td> :
 										<td>
-											<select name="attendance" id={boy.id} value={currentAttendance[boy.id] || ''} onChange={(e) => setAttendance(boy.id, e.target.value)} onClick={() => console.log(boy.id)} onFocus={() => setSelectedInput(boy.id)} onKeyDown={e => handleKeyDown(e, boy.id)} style={{ backgroundColor: selectedInput === boy.id ? "lightgrey" : "white" }} >
+											<select name="attendance" id={boy.id} value={currentAttendance[boy.id] || ''} onChange={(e) => setAttendance(boy.id, e.target.value)} onFocus={() => setSelectedInput(boy.id)} onKeyDown={e => handleKeyDown(e, boy.id)} style={{ backgroundColor: selectedInput === boy.id ? "lightgrey" : "white" }} >
 												<option value="">-</option>
 												<option value="1">1</option>
 												<option value="S">S</option>
