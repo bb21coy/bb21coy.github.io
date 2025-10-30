a5ffa4906099f06bad2f7059a20633ba5b238e60
Dylan <dylanyeowf@gmail.com>
2025-10-30 21:55:22 +0800

    redesigned login page

--- public/sw.js
+++ public/sw.js
@@ -1,4 +1,4 @@
-const version = "1.5.19";
+const version = "1.6.19";
 const CACHE_NAME = `bb21coy-cache-v${version}`;
 
 // These are the known root files and folders
--- src/main.jsx
+++ src/main.jsx
@@ -38,10 +38,10 @@ createRoot(document.body).render(
 				<Suspense fallback={<Loading />}>
 					<UserProvider>
 						<Routes>
+							<Route path='/login' element={<LogInPage />} />
 							<Route element={<Layout />}>
 								{/* Temporarily disable home page since its filled with filler data */}
 								{/* <Route path='/' element={<HomePage/>}/> */}
-								<Route path='/login' element={<LogInPage />} />
 								<Route path='/' element={<LogInPage />} />
 								<Route path='/parade_notice' element={<ParadeNoticePage />} />
 								
